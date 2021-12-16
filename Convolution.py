6ea1a15fc580ea97a8f5e2190a680b3ec8de8a98
cmwahl <54952987+cmwahl@users.noreply.github.com>
2021-12-15 23:34:33 -0500

    Add files via upload
    Made my convolution function implement trapezoid method for the integration instead of the forward rectangle approximation.

--- Convolution.py
+++ Convolution.py
@@ -4,7 +4,7 @@ from math import *
 import matplotlib.pyplot as plt
 
 
-def convolution(impulse_response_func, signal_func, t_tot=10, t_delta=0.1):
+def convolution(impulse_response_func, signal_func, t_tot=10, t_delta=0.01):
     '''
     Given two functions with parameter 't' for time, returns a list of values representing the
     signal response over time
@@ -14,14 +14,20 @@ def convolution(impulse_response_func, signal_func, t_tot=10, t_delta=0.1):
     :param t_delta: Delta time between discrete points in time (d_Tau)
     :return: List of convoluted values over time
     '''
-    impulse_reponse_vals = [impulse_response_func(t * t_delta) for t in range(int(t_tot/t_delta))]
-    signal_vals = [signal_func(t * t_delta) for t in range(int(t_tot/t_delta))]
+
+    # Note, adding cushion 0 to beginning and an extra point at the end to make trapezoid method easier
+    impulse_reponse_vals = [impulse_response_func(t * t_delta) for t in range(int(t_tot/t_delta + 1))]
+    impulse_reponse_vals.insert(0, 0)
+
+    signal_vals = [signal_func(t * t_delta) for t in range(int(t_tot/t_delta) + 1)]
+    signal_vals.insert(0, 0)
+
     convolution = [0 for t in range(int(t_tot/t_delta))]
 
     area = 0
     for num_points_to_use in range(int(t_tot/t_delta)):
         for index in range(num_points_to_use + 1):
-            area = area + signal_vals[index] * impulse_reponse_vals[num_points_to_use - index] * t_delta
+            area = area + (signal_vals[index] + signal_vals[index + 2]) * (impulse_reponse_vals[num_points_to_use - index] + impulse_reponse_vals[num_points_to_use - index + 2]) * t_delta / 4
         convolution[num_points_to_use] = area
         area = 0
 
@@ -50,8 +56,8 @@ def test_decay(t):
 
 if __name__ == "__main__":
     t_tot = 10
-    t_delta = 0.1
-    times = [t * 0.1 for t in range(int(t_tot/t_delta))]
+    t_delta = 0.01
+    times = [t * t_delta for t in range(int(t_tot/t_delta))]
 
     plt.plot(times, [test_rect(t * t_delta) for t in range(int(t_tot/t_delta))])
     plt.plot(times, convolution(test_rect, test_rect, t_tot=t_tot, t_delta=t_delta))
